020efb948380c996f3a2be1e83a3d65719d621a0
AlinaCherkasova <alina.ch1202@gmail.com>
2018-08-08 12:36:12 +0300

    form input

--- src/js/common.js
+++
@@ -1,5 +0,0 @@
-var clickElem = document.getElementById('main');
-clickElem.addEventListener('click',function(e){
-    document.getElementById('click').style.display = 'inline';
-    clickElem.style.display = 'none';
-})
